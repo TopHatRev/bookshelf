2fde4ea5818199c1fb2502409dff11a2b908caf8
Dylan Blackwell <tophatrev@gmail.com>
2023-03-14 16:36:40 +0000

    Add input fields and CSS

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div>
+      <div className="header-bar">
+        <h1>Bookshelf</h1>
+      </div>
+    </div>
+  );
+}
