efb1757ffadb404f16ba1e41ed64175b185679b7
Hayden Kerr <kerrster223@gmail.com>
2023-03-14 13:39:15 +0000

    added nodemon body-parser and server things

--- server/seed.js
+++ server/seed.js
@@ -9,19 +9,13 @@ async function seed() {
   // create a new instance of a cat object and add to the DB
   await Book.create({
     title: "Gone",
+    author: "Micheal Grant",
+    year: "2008",
+    countryOfOrigin: "Usa",
     description: "action book",
     status: "yes",
   });
-  await Book.create({
-    name: "American psycho",
-    description: "rich man does bad",
-    status: "yes",
-  });
-  await Book.create({
-    name: "Salo or the 120 days of sodom",
-    description: "4 rich men buy people",
-    status: "yes",
-  });
+
   console.log("Created a new book");
 
   mongoose.disconnect();
